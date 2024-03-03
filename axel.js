279e41dc16f913423cc466e2e849b67a42795954
Axel Collin <shoulder_87@hotmail.com>
2024-03-03 22:32:25 +0100

    added functionality to save to local storage, included some code to load from json from the other project, all done

--- axel.js
+++ axel.js
@@ -1,9 +1,11 @@
 const questionsContainer = document.getElementById("questions-container");
 const addQuestion = document.getElementById("add-question");
 const saveQuiz = document.getElementById("save-quiz");
-
+const loadQuiz = document.getElementById("load-quiz");
+const submitQuiz = document.getElementById("submit-quiz");
 let questionsArr = [];
 let qNumber = 1;
+localStorage.clear();
 
 addQuestion.addEventListener("click", () => {
     const html = `
@@ -28,6 +30,7 @@ addQuestion.addEventListener("click", () => {
         </div>
         `;
     questionsArr.push(qNumber);
+    console.log(qNumber);
     qNumber += 1;
     saveQuiz.disabled = false;
     questionsContainer.insertAdjacentHTML("beforeend", html);
@@ -59,7 +62,7 @@ questionsContainer.addEventListener("click", (event) => {
                     <label>Alternative:</label>
                     <br />
                     <div id='alternative'>
-                        <input id="alternative${event.target.name}" type="text" data-linked-radio="radio1"/>
+                        <input id="alternative${event.target.name}" type="text" data-linked-radio="radio2"/>
                         <input id="radio2" name="alternative${event.target.name}" type="radio" />
                         <label>Answer</label>
                         <br />
@@ -73,16 +76,16 @@ questionsContainer.addEventListener("click", (event) => {
                     <label>Alternative:</label>
                     <br />
                     <div id='alternative'>
-                        <input type="text" />
-                        <input name="alternative${event.target.name}" type="checkbox" />
+                        <input id="alternative${event.target.name}" type="text" data-linked-checkbox="checkbox1" />
+                        <input id="checkbox1" name="alternative${event.target.name}" type="checkbox" />
                         <label>Answer</label>
                         <br />
                     </div>
                     <label>Alternative:</label>
                     <br />
                     <div id='alternative'>
-                        <input type="text" />
-                        <input name="alternative${event.target.name}" type="checkbox" />
+                        <input id="alternative${event.target.name}" type="text" data-linked-checkbox="checkbox2" />
+                        <input id="checkbox2" name="alternative${event.target.name}" type="checkbox" />
                         <label>Answer</label>
                         <br />
                     </div>
@@ -96,14 +99,12 @@ questionsContainer.addEventListener("click", (event) => {
         console.log(radioId);
 
         const alternatives = document.querySelectorAll(
-            `input[name="alternative${radioId}"]`
+            `input[id="alternative${radioId}"]`
         );
 
-        console.log(alternatives);
+        alternatives.forEach((ele) => console.log(ele.value));
 
         const numberOfAlternatives = alternatives.length;
-
-        console.log(numberOfAlternatives);
 
         const targetDiv = document.getElementById(
             `alternative-container-${radioId}`
@@ -126,7 +127,12 @@ questionsContainer.addEventListener("click", (event) => {
         targetDiv.insertAdjacentHTML("beforeend", html);
     } else if (event.target.id.startsWith("add-checkbox-")) {
         let checkId = event.target.id.charAt(event.target.id.length - 1);
-        console.log(checkId);
+
+        const alternatives = document.querySelectorAll(
+            `input[id="alternative${checkId}"]`
+        );
+
+        const numberOfAlternatives = alternatives.length;
 
         const targetDiv = document.getElementById(
             `alternative-container-${checkId}`
@@ -135,15 +141,21 @@ questionsContainer.addEventListener("click", (event) => {
         let html = `
             <label>Alternative:</label>
             <div id='alternative'>
-                <input type="text" />
-                <input name="alternative${checkId}" type="checkbox" />
+                <input id="alternative${checkId}" type="text" data-linked-checkbox="checkbox${
+            numberOfAlternatives + 1
+        }" />
+                <input id="checkbox${
+                    numberOfAlternatives + 1
+                }" name="alternative${checkId}" type="checkbox" />
                 <label>Answer</label>
                 <br />
             </div>
         `;
         targetDiv.insertAdjacentHTML("beforeend", html);
     } else if (event.target.id.startsWith("remove")) {
-        const targetId = event.target.id.charAt(event.target.id.length - 1);
+        const targetId = parseInt(
+            event.target.id.charAt(event.target.id.length - 1)
+        );
         const removeTarget = document.getElementById(
             `content-container-${targetId}`
         );
@@ -162,24 +174,151 @@ function toggleSaveButton() {
 }
 
 saveQuiz.addEventListener("click", () => {
+    localStorage.clear();
+    console.log(questionsArr);
+    // For each question, check type and then collect data depending on the type
     questionsArr.forEach((question) => {
-        // Fråga:
-        console.log(document.getElementById(`quiz-question-${question}`).value);
+        let questionObject = {
+            id: question,
+            question: document.getElementById(`quiz-question-${question}`)
+                .value,
+        };
 
         const checkedRadio = document.querySelector(
             `input[name='${question}']:checked`
         );
 
+        const alternatives = document.querySelectorAll(
+            `input[id="alternative${question}"]`
+        );
+
         switch (checkedRadio.value) {
             case "text":
-                console.log(document.getElementById(`answer${question}`).value);
+                questionObject.type = "text";
+                questionObject.answer = document.getElementById(
+                    `answer${question}`
+                ).value;
                 break;
             case "radio":
-                console.log("radio");
+                questionObject.type = "radio";
+
+                questionObject.options = [];
+
+                alternatives.forEach((ele) =>
+                    questionObject.options.push(ele.value)
+                );
+
+                const checkedAnswerRadio = document.querySelector(
+                    `input[name="alternative${question}"]:checked`
+                );
+
+                const answerInput = document.querySelector(
+                    `input[data-linked-radio="${checkedAnswerRadio.id}"]`
+                );
+
+                questionObject.answer = answerInput.value;
+
                 break;
             case "check":
-                console.log("check");
+                questionObject.type = "checkbox";
+
+                questionObject.options = [];
+
+                alternatives.forEach((ele) =>
+                    questionObject.options.push(ele.value)
+                );
+
+                const checkedBoxes = document.querySelectorAll(
+                    `input[name="alternative${question}"]:checked`
+                );
+
+                questionObject.answer = [];
+
+                checkedBoxes.forEach((cb) => {
+                    const linkedInput = document.querySelector(
+                        `input[data-linked-checkbox="${cb.id}"]`
+                    );
+
+                    if (linkedInput) {
+                        // Log the value of the linked text input
+                        questionObject.answer.push(linkedInput.value);
+                    }
+                });
                 break;
         }
+        saveQuizLocaly(questionObject);
+        document.getElementById(`content-container-${question}`).innerHTML = "";
+        document.getElementById(`content-container-${question}`).style.padding =
+            "0";
+        document.getElementById(`content-container-${question}`).style.margin =
+            "0";
+        saveQuiz.style.display = "none";
+        addQuestion.style.display = "none";
+        loadQuiz.style.display = "inline";
     });
+});
+
+function saveQuizLocaly(questionObject) {
+    let existingQuestions = localStorage.getItem("questions");
+    let questions = [];
+
+    if (existingQuestions) {
+        questions = JSON.parse(existingQuestions);
+    }
+    questions.push(questionObject);
+    console.log(questions);
+    localStorage.setItem("questions", JSON.stringify(questions));
+}
+
+loadQuiz.addEventListener("click", () => {
+    let storedQuestions = localStorage.getItem("questions");
+    let questionCount = 1;
+    let questions = JSON.parse(storedQuestions);
+
+    questions.forEach((q) => {
+        // Start of the question div
+
+        let questionHtml = `<div class="question" id="q${q.id}">`;
+
+        switch (q.type) {
+            case "text":
+                questionHtml += `<p class="questionP">${questionCount}. ${q.question}</p>`;
+                questionHtml += `<input type='text' name='${q.id}' placeholder='Type answer here (required) '/>`;
+                questionHtml += `<p class="input-error" id='${q.id}'></p>`;
+                questionCount += 1;
+                break;
+            case "radio":
+                questionHtml += `<p class="questionP">${questionCount}. ${q.question}</p>`;
+                q.options.forEach((option) => {
+                    questionHtml += `<input type='radio' name='${q.id}' value='${option}'/>`;
+                    questionHtml += `<label for='${option}'>${option}</label>`;
+                    questionHtml += `<br />`;
+                });
+                questionHtml += `<p class="input-error" id='${q.id}'></p>`;
+                questionCount += 1;
+                break;
+            case "checkbox":
+                questionHtml += `<p class="questionP">${questionCount}. ${q.question} (Multi choice)</p>`;
+                q.options.forEach((option) => {
+                    questionHtml += `<input type='checkbox' name='${q.id}' value='${option}'/>`;
+                    questionHtml += `<label for='${option}'>${option}</label>`;
+                    questionHtml += `<br />`;
+                });
+                questionHtml += `<p class="input-error" id='${q.id}'></p>`;
+                questionCount += 1;
+                break;
+        }
+
+        // End of the question div
+        questionHtml += `</div>`;
+
+        // Append the entire question div to the container
+        questionsContainer.innerHTML += questionHtml;
+        loadQuiz.style.display = "none";
+        submitQuiz.style.display = "flex";
+    });
+});
+
+submitQuiz.addEventListener("click", () => {
+    alert("Congratulations on completing the quiz!");
 });
